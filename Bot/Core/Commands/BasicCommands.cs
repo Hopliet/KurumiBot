c1113f9e2ed76b9281a7705904d497b6070f808b
Hop <michiel.vanmol@homail.com>
2019-04-10 23:10:53 +0200

    cleaning up case sensitive commands

--- Bot/Core/Commands/BasicCommands.cs
+++ Bot/Core/Commands/BasicCommands.cs
@@ -11,19 +11,19 @@ namespace DiscordBot.Core.Commands
     public class Ping : ModuleBase<SocketCommandContext/*comes from MessageReceived*/>
     {
         //specifying a command in discord
-        [Command("Ping"), Alias("Check", "ping"), Summary("Test command")]
+        [Command("Ping"), Alias("Check"), Summary("Test command")]
         public async Task ReplyPing()
         {
             await Context.Channel.SendMessageAsync("ara ara, I am working now");
         }
 
-        [Command("baka"), Alias("Baka", "BAKA", "bAKA"), Summary("Baka command")]
+        [Command("baka"), Summary("Baka command")]
         public async Task ReplyBaka()
         {
             await Context.Channel.SendMessageAsync($"No, {Context.User.Mention} is");
         }
 
-        [Command("stuck"), Alias("sensei", "Stuck", "Sensei", "Mentor", "mentor")]
+        [Command("stuck"), Alias("sensei", "Sensei", "mentor")]
         public async Task MentionSensei()
         {
             await Context.Channel.SendMessageAsync($"{Context.Guild.GetRole(561965462918004737).Mention}");
