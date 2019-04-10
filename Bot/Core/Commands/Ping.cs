5ca88de339474461b5caadb14c61a0e0ac775284
Hop <michiel.vanmol@homail.com>
2019-04-10 22:49:48 +0200

    Cleaning up function names

--- Bot/Core/Commands/Ping.cs
+++ Bot/Core/Commands/Ping.cs
@@ -12,21 +12,21 @@ namespace DiscordBot.Core.Commands
     {
         //specifying a command in discord
         [Command("Ping"), Alias("Check", "ping"), Summary("Test command")]
-        public async Task RunTestCommand()
+        public async Task ReplyPing()
         {
             await Context.Channel.SendMessageAsync("ara ara, I am working now");
         }
 
         [Command("baka"), Alias("Baka", "BAKA", "bAKA"), Summary("Baka command")]
-        public async Task RunBaka()
+        public async Task ReplyBaka()
         {
             await Context.Channel.SendMessageAsync($"No, {Context.User.Mention} is");
         }
 
-        [Command("stuck"), Alias("sensei", "Stuck", "Sensei")]
+        [Command("stuck"), Alias("sensei", "Stuck", "Sensei", "Mentor", "mentor")]
         public async Task MentionSensei()
         {
-            await Context.Channel.SendMessageAsync($"{Context.Guild.GetRole(562022584917426177).Mention}");
+            await Context.Channel.SendMessageAsync($"{Context.Guild.GetRole(561965462918004737).Mention}");
         }
     }
 }
