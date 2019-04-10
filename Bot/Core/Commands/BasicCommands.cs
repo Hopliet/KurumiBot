b35655798d9f42a70e4f7b5d87cc70088dd93500
Hop <michiel.vanmol@homail.com>
2019-04-10 23:22:20 +0200

    Fixing BasicCommands.cs

--- Bot/Core/Commands/BasicCommands.cs
+++ Bot/Core/Commands/BasicCommands.cs
@@ -11,10 +11,22 @@ namespace DiscordBot.Core.Commands
     public class Ping : ModuleBase<SocketCommandContext/*comes from MessageReceived*/>
     {
         //specifying a command in discord
-        [Command("Ping"), Alias("Check", "ping"), Summary("Test command")]
-        public async Task RunTestCommand()
+        [Command("Ping"), Alias("Check", "ping"), Summary("Runs a modified ping command")]
+        public async Task ReplyPing()
         {
             await Context.Channel.SendMessageAsync("ara ara, I am working now");
+        }
+
+        [Command("Baka"), Summary("Shows who the real baka is")]
+        public async Task ReplyBaka()
+        {
+            await Context.Channel.SendMessageAsync($"no, {Context.User.Mention} is!");
+        }
+
+        [Command("Sensei"), Alias("Mentor", "Stuck", "support")]
+        public async Task MentionMentor()
+        {
+            await Context.Channel.SendMessageAsync(Context.Guild.GetRole(561965462918004737).Mention);
         }
     }
 }
